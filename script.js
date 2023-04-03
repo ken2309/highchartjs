75ed2eeab3294b53db74d6717f4d97377352660b
NgQuangKhai <khai@saigon.digital>
2023-04-04 00:15:15 +0700

    Init commit

---
+++ script.js
@@ -0,0 +1,67 @@
+const seriesList = [
+  // {
+  //   name: "Series 1", data: [
+  //     [0, 4, 6, 10, 12],
+  //     [1, 3, 5, 7, 10],
+  //     [2, 4, 6, 8, 10]
+  //   ], type: 'boxplot'
+  // },
+  {
+    name: "Series 2", data: [
+      [0, 4, 6, 13, 18],
+      [1, 7, 9, 14, 16],
+      [2, 4, 10, 16, 20]
+    ], type: 'boxplot'
+  },
+  {
+    name: "Series 3", data: [
+      [0, 4],
+      [0, 7],
+      [0, 13],
+      [1, 3],
+      [2, 4]
+    ], type: 'scatter'
+  },
+];
+
+const chartOptions = {
+  chart: {
+    type: "boxplot",
+    renderTo: "chart-container",
+  },
+  legend: {
+    enabled: true
+  },
+  series: [],
+};
+
+const chart = new Highcharts.Chart(chartOptions);
+
+const seriesListElement = document.getElementById("series-list");
+seriesList.forEach((series, index) => {
+  const li = document.createElement("li");
+  li.textContent = series.name;
+  li.addEventListener("click", () => {
+    const seriesOptions = {
+      name: series.name,
+      data: series.data,
+      type: series.type
+    };
+
+    chart.addSeries(seriesOptions);
+
+    const allSeries = chart.series;
+    console.log(allSeries);
+    const boxplotSeries = allSeries.find((s) => s.userOptions.type === "boxplot");
+    const scatterSeries = allSeries.find((s) => s.userOptions.type === "scatter");
+    console.log(boxplotSeries, scatterSeries);
+    if (boxplotSeries && scatterSeries) {
+      scatterSeries.points.forEach((point, index) => {
+        const boxplotPoint = boxplotSeries.points[index];
+        if(boxplotPoint) point.graphic.element.transform = boxplotPoint.graphic.element.transform;
+      });
+    }
+  });
+
+  seriesListElement.appendChild(li);
+});
